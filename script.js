43569102acc05c634edd7c9e7913c5ab8eb27654
khatripramod <pramod.khatri@thamescollege.edu.np>
2025-05-07 21:05:50 +0545

    form done

--- script.js
+++ script.js
@@ -42,12 +42,8 @@ document.querySelector('.scroll-indicator').addEventListener('click', function()
     });
 });
 
-// Form submission handling
-document.querySelector('.contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for your message! We will get back to you soon.');
-    this.reset();
-});
+
+
 
 // Add animation to service cards on scroll
 const observerOptions = {
